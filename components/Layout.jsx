dd97b19f316170f4de1734898e28a9598d744ba5
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2022-07-14 18:54:32 +0100

    update: increase the max width

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Layout = ({ children, ...others }) => {
   return (
-    <Box maxWidth="2040px" mx="auto" {...others} overflowX="hidden">
+    <Box maxWidth="4040px" mx="auto" {...others} overflowX="hidden">
       {children}
     </Box>
   );
